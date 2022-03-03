f4eb3f66b9b414ddba4e1d35eaef16170eeb83a6
Bhavika Setpal <bhavikasetpal123@gmail.com>
2022-03-03 13:06:19 +0530

    my first release

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplateformComponent } from './templateform/templateform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'templateform',component:TemplateformComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
